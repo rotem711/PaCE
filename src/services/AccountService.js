664f854317fe46256ae7c9398c4ea2059ff9bcdb
rotem711 <rotemskandrani@gmail.com>
2021-05-11 16:44:27 -0400

    update api service for account

--- src/services/AccountService.js
+++ src/services/AccountService.js
@@ -1,29 +1,29 @@
 import APIService from "@/http.js";
 const API = new APIService();
 
-export default class AccountService {
+export default {
   async register (payload) {
     let res = await API.post('/api/users/register', payload)
     return res.data;
-  }
+  },
 
   async forgotPassword (payload) {
     let headers = { "Content-Type": "application/json-patch+json" };
     let res = await API.post("/api/accounts/forgotPassword", payload, headers);
     return res.data;
-  }
+  },
 
   async resetPass (payload) {
     let headers = { "Content-Type": "application/json-patch+json" };
     let res = await API.post("/api/accounts/resetPass", payload, headers);
     return res.data;
-  }
+  },
 
   async resetPassword (payload) {
     let headers = { "Content-Type": "application/json-patch+json" };
     let res = await  API.post("/api/accounts/resetPassword", payload, headers);
     return res.data;
-  }
+  },
 
   async changePassword (payload) {
     let headers = { "Content-Type": "application/json-patch+json" };
