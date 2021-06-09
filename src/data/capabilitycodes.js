009689bc5542420f649f013490c78e51d013a4bd
rotem711 <rotemskandrani@gmail.com>
2021-06-08 23:27:56 -0400

    update text in capability codes

--- src/data/capabilitycodes.js
+++ src/data/capabilitycodes.js
@@ -14,7 +14,7 @@ const capabilityCodes = [{
 }, {
     id: 3,
     name: "Care Worker",
-    short: "HC",
+    short: "CW",
     items: [{
         description: "Person-centred communication to build trust, show empathy and demonstrate support for individuals and carers living with a life-limiting illness."
     }, {
